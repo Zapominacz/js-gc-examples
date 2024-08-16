e980a54a6e9ed1f6f410d1dcf59b158d488ebf6a
Mikołaj Wilczek <mikolaj@kiwicode.dev>
2024-08-16 14:08:13 +0200

    Approach #2 - WeakRef

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,15 @@
 'use client'
 
-import { useState, useCallback, useEffect } from "react";
+import { useState, useCallback } from "react";
 
 class BigObject {
   data = new Uint8Array(1024 * 1024 * 10);
 }
-
- // this will event make the error for the first use
 const Home = () => {
   const [countA, setCountA] = useState(0);
   const [countB, setCountB] = useState(0);
-  const weakMap = new WeakMap();
-  let key = {}; // unique key
-  weakMap.set(key, new BigObject())
-  useEffect(() => {
-    return () => {
-      console.log("cleanup");
-      key = null;
-      weakMap.delete(key);
-    };
-  }, [])
-
+  const bigObject = new BigObject();
+  const weakRef = new WeakRef(bigObject);
   const handleClickA = useCallback(() => {
     setCountA(countA + 1);
   }, [countA]);
@@ -33,11 +22,12 @@ const Home = () => {
   const handleClickBoth = () => {
     handleClickA();
     handleClickB();
-    console.log(weakMap.get(key)?.data.length);
+    console.log(weakRef.deref()?.data.length); // can be undefined or 10 485 760
   };
 
   return (
     <div>
+      <span>{bigObject.data.length}</span>
       <button onClick={handleClickA}>Increment A</button>
       <button onClick={handleClickB}>Increment B</button>
       <button onClick={handleClickBoth}>Increment Both</button>
